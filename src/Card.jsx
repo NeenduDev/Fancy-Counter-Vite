1a06385629a6274592a02864d6e46e9a1367de99
NeenduDev <neendu.wickremasinghe@gmail.com>
2025-02-13 15:58:54 +0530

    feat: Implement 'Increase' feature with Spacebar key press

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,24 +2,35 @@ import ResetButton from "./ResetButton";
 import Count from "./Count";
 import ButtonContainer from "./ButtonContainer";
 import Title from "./Title";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CountButton from "./CountButton";
 
 export default function Card() {
   const [count, setCount] = useState(0);
   const isLocked = count > 5;
-  function handleIncrease() {
+  function handleIncrease(e) {
     // if (count === 5) return;
     setCount((c) => (c > 5 ? 6 : c + 1));
+    e.currentTarget.blur();
   }
-  function handleDecrease() {
+  function handleDecrease(e) {
     if (count === 0) return;
     setCount((c) => c - 1);
+    e.currentTarget.blur();
   }
 
-  function handleReset() {
+  function handleReset(e) {
     setCount(0);
+    e.currentTarget.blur();
   }
+
+  useEffect(() => {
+    const handleKeyDown = (e) => {
+      if (e.code === "Space") setCount((c) => (c > 5 ? 6 : c + 1));
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
 
   return (
     <div className={count > 5 ? `card card--limit` : `card`}>
